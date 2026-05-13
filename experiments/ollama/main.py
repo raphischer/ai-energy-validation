9539b5a8f9de30e2197160bcc228274d61b216ae
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2026-05-13 12:56:01 +0000

    improved ollama code

--- experiments/ollama/main.py
+++ experiments/ollama/main.py
@@ -12,11 +12,11 @@ from lamarr_energy_tracker.tracker import EnergyTracker
 
 from util import print_colored_block, get_processor_name, get_gpu_name, save_webcam_image
 
-def read_queries(random=True):
+def read_queries(random=42):
     conversations = pd.read_json(path_or_buf=os.path.join(os.path.dirname(__file__), 'llm_baseline_conversations_puffin.jsonl'), lines=True)
     conversations.set_index('id', inplace=True)
     if random:
-        conversations = conversations.sample(frac=1)
+        conversations = conversations.sample(frac=1, random_state=random)
     return [conv[0]['value'] for conv in conversations['conversations']]
 
 def parse_param_count(s):
@@ -39,9 +39,11 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Inference benchmarking with ollama LLMs")
     # data and model input
-    parser.add_argument("--model", default="gemma3:1b")
-    parser.add_argument('--temperature', type=float, default=0.7)
-    parser.add_argument("--nogpu", type=int, default=0)
+    parser.add_argument("--model", default="gemma3:1b", help="ollama model to benchmark")
+    parser.add_argument('--temperature', type=float, default=0.7, help='temperature for sampling, default 0.7')
+    parser.add_argument("--nogpu", type=int, default=0, help="disable gpu usage for inference, set to 1 to disable")
+    parser.add_argument("--random", type=int, default=42, help="random seed for shuffling the queries, set to 0 to disable shuffling")
+    parser.add_argument("--timeout", type=int, default=120, help="timeout for each inference call in seconds")
     parser.add_argument("--seconds", type=int, default=900, help="number of seconds to profile model on a subset of the data -- 0 process complete")
     args = parser.parse_args()
     mlflow.log_dict(args.__dict__, 'config.json')
@@ -62,7 +64,7 @@ if __name__ == '__main__':
         os.environ["OLLAMA_NO_GPU"] = "1"
     
     # load data
-    queries = read_queries()
+    queries = read_queries(random=args.random)
 
     # delete old models (for freeing disk space), load new model and its meta info, init inference (often has some temporal overhead)
     import ollama
@@ -71,12 +73,13 @@ if __name__ == '__main__':
             for mod in ollama.list().models:
                 ollama.delete(mod.model)
     ollama.pull(args.model)
-    resp = ollama.chat(model=args.model, messages=[{"role": "user", "content": f"Can you answer questions?"}])
+    client = ollama.Client(host='http://localhost:11434', timeout=args.timeout)
+    resp = client.chat(model=args.model, messages=[{"role": "user", "content": f"Can you answer questions?"}])
     mlflow.log_param('file_size', ollama.list().models[0].size)
     mlflow.log_param('parameters', parse_param_count(ollama.list().models[0].details.parameter_size))
 
     # prepare evaluations
-    times, n_samples, tokens = [], 0, {'in': [], 'out': []}
+    times, failed_times, tokens = [], [], {'in': [0], 'out': [0]}
     gt_tracker = GroundTruthTracker(verbose=False)
     tracker = EnergyTracker(output_dir=os.getcwd())
     gt_tracker.start()
@@ -88,26 +91,31 @@ if __name__ == '__main__':
     # run inference
     for query in queries:
         t0 = time.time()
-        resp = ollama.chat(model=args.model, messages=[{"role": "user", "content": query}], options={"temperature": args.temperature})
         try:
+            resp = client.chat(model=args.model, messages=[{"role": "user", "content": query}], options={"temperature": args.temperature})
             tokens['in'].append(resp['prompt_eval_count'])
             tokens['out'].append(resp['eval_count'])
-        except:
-            pass
+        except Exception as e:
+            failed_times.append(time.time() - t0)
         times.append(time.time() - t0)
-        n_samples += 1
-        remaining = args.seconds - sum(times) if args.seconds and len(times) < 5 else args.seconds - (sum(times) + np.average(times))
-        print(f"\rProcessed queries: {n_samples} | Remaining time: {remaining:.1f}s", end='', flush=True)
+        remaining = args.seconds - (sum(times) + np.average(times))
+        print(f"\rProcessed queries: {len(times):<3} | Remaining time: {remaining:.1f}s | mean time: {np.average(times):.2f}s | std time: {np.std(times):.2f}s  | max time: {np.max(times):.2f}s | errors: {len(failed_times)}", end='', flush=True)
         if args.seconds and remaining < 0:
             break
-    print_colored_block(f'STOPPING ENERGY PROFILING FOR   {args.model.upper()}  temperature {args.temperature} on   {"CPU" if args.nogpu else "GPU"}', ok=False)
+    print_colored_block(f'STOPPING ENERGY PROFILING FOR  {args.model.upper()}  temperature {args.temperature} on   {"CPU" if args.nogpu else "GPU"}', ok=False)
     eval_gt = gt_tracker.stop()
     tracker.stop(print_summary=False)
     save_webcam_image("capture_stop.jpg")
 
-    # add average amount of tokens if there we any errors:
-    tokens['in'] += [np.mean(tokens['in'])] * (n_samples - len(tokens['in']))
-    tokens['out'] += [np.mean(tokens['out'])] * (n_samples - len(tokens['in']))
+    print(f"\rProcessed queries: {len(times):<3} | Remaining time: {remaining:.1f}s | mean time: {np.average(times):.2f}s | std time: {np.std(times):.2f}s  | max time: {np.max(times):.2f}s | errors: {len(failed_times)}")
+
+    if failed_times: # add estimates for untracked token counts if there we any timeouts:
+        time_with_tokens = sum(times) - sum(failed_times)
+        time_without_tokens = sum(failed_times)
+        in_tokens_per_s = sum(tokens['in']) / time_with_tokens
+        out_tokens_per_s = sum(tokens['out']) / time_with_tokens
+        tokens['in'].append(in_tokens_per_s * time_without_tokens)
+        tokens['out'].append(out_tokens_per_s * time_without_tokens)
 
     # aggregate results
     emissions = 'emissions.csv'
@@ -115,20 +123,23 @@ if __name__ == '__main__':
     results = {
         'n_tokens_in': sum(tokens['in']),
         'n_tokens_out': sum(tokens['out']),
+        'avg_time': np.average(times),
+        'max_time': np.max(times),
+        'std_time': np.std(times),
         # codecarbon logs
         'running_time_total': emission_data['duration'][0],
-        'running_time':  emission_data['duration'][0] / n_samples,
+        'running_time':  emission_data['duration'][0] / sum(tokens['out']),
         'power_draw_total': emission_data['energy_consumed'][0] * 3.6e6,
-        'power_draw': emission_data['energy_consumed'][0] * 3.6e6 / n_samples,
+        'power_draw': emission_data['energy_consumed'][0] * 3.6e6 / sum(tokens['out']),
         # ground-truth logs
         'power_draw_total_gt': eval_gt['energy_consumed'] * 3.6e6,
-        'power_draw_gt': eval_gt['energy_consumed'] * 3.6e6 / n_samples,
+        'power_draw_gt': eval_gt['energy_consumed'] * 3.6e6 / sum(tokens['out']),
         'running_time_total_gt': eval_gt['duration'],
-        'running_time_gt':  eval_gt['duration'] / n_samples
+        'running_time_gt':  eval_gt['duration'] / sum(tokens['out'])
     }
 
     # log results & cleanup
-    mlflow.log_param('n_samples', n_samples)
+    mlflow.log_param('n_samples', len(times))
     for key, val in results.items():
         mlflow.log_metric(key, val)
     for f in [emissions, 'capture_start.jpg', 'capture_stop.jpg']:
@@ -137,5 +148,4 @@ if __name__ == '__main__':
             os.remove(f)
     mlflow.end_run()
     print(results)
-    print('n_samples', n_samples)
     sys.exit(0)
--- experiments/ollama/run_all.sh
+++ experiments/ollama/run_all.sh
@@ -6,6 +6,7 @@ exp_name="ollama_$now"
 exp_create_str=$(mlflow experiments create -n $exp_name)
 echo $exp_create_str
 exp_id=$(echo $exp_create_str | awk '{print $NF}')
+seed=$RANDOM
 
 # Define model architectures to iterate over # replace 8b with 14b
 models=("gpt-oss:20b" "deepseek-r1:8b" "deepseek-r1:1.5b"
@@ -19,9 +20,9 @@ models=("gpt-oss:20b" "deepseek-r1:8b" "deepseek-r1:1.5b"
 # Loop over all models and GPUs
 for m in "${models[@]}"
 do
-    for t in "0.1" "0.7"
+    for t in "0.0" "0.7"
     do
-        mlflow run --experiment-name=$exp_name -e main.py -P model=$m -P temperature=$t ./experiments/ollama
+        mlflow run --experiment-name=$exp_name -e main.py -P model=$m -P temperature=$t -P random=$seed ./experiments/ollama
         # Save experiment data to CSV
         mlflow experiments csv -x $exp_id > "results/$exp_name.csv"
     done
